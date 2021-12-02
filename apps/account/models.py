93178e8923795aa93a5f391700737cb211e2bcab
Danil Vashenko <danil.vashenko.44@gmail.com>
2021-12-02 12:05:16 +0400

    Переделаны миграции, обновлена модель user

--- apps/account/models.py
+++ apps/account/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
+    email = models.EmailField('Адрес электронной почты', unique=True)
 
     class Meta:
         verbose_name = 'Пользователь'
